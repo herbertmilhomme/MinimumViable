39adad99ea8c2f78f70b1513373f8a3ebf24cc2e
herbertmilhomme <herbertmilhomme@gmail.com>
2016-08-15 22:45:29 -0400

    Consolidate
    Removed deprecated and obsolete projects, to clean and organize new work
    area.

---
+++ Minimum4/Assets/Scripts/TerrainEnvirnment/WaveTypes.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Different wavetypes
+public class WaveTypes
+{
+
+    //Sinus waves
+    public static float SinXWave(
+        Vector3 position,
+        float speed,
+        float scale,
+        float waveDistance,
+        float noiseStrength,
+        float noiseWalk,
+        float timeSinceStart)
+    {
+        float x = position.x;
+        float y = 0f;
+        float z = position.z;
+
+        //Using only x or z will produce straight waves
+        //Using only y will produce an up/down movement
+        //x + y + z rolling waves
+        //x * z produces a moving sea without rolling waves
+
+        float waveType = z;
+
+        y += Mathf.Sin((timeSinceStart * speed + waveType) / waveDistance) * scale;
+
+        //Add noise to make it more realistic
+        y += Mathf.PerlinNoise(x + noiseWalk, y + Mathf.Sin(timeSinceStart * 0.1f)) * noiseStrength;
+
+        return y;
+    }
+}
